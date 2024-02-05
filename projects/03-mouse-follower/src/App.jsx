e021392d2b388def8c0055032236844e4b8da19c
CDavidDv <carlos.david1806@gmail.com>
2024-02-05 15:06:42 -0600

    third project

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [enable, setEnable] = useState(false)
+
+  useEffect(() => {
+    setEnable(true)
+  }, [])
+
+
   return (
     <>
      <h3>Proyecto 3</h3>
+     <button onClick={() => setEnable(!enable)}>{enable ?  'Desactivar' : 'Activar'} seguir puntero</button>
     </>
   )
 }
